5744d7674f40473a59179cb7d1b1b05a319264af
Carol Schluter <cfmiller176@gmail.com>
2023-02-07 17:31:02 -0600

    submit event handling

--- food-lookup.js
+++ food-lookup.js
@@ -1,3 +1,15 @@
-// https://www.themealdb.com/
+const form = document.querySelector('.js-food-form');
+const foodContainer= document.querySelector('.js-food-container');
 
-console.log ('JvaScript is running');
+
+// https://www.themealdb.com/api/json/v1/1/search.php?s=Pie
+
+console.log('JavaScript is running');
+
+function callback(event) {
+    event.preventDefault();
+    alert('form got submitted');
+}
+
+form.addEventListener('submit', callback);
+
