362b7af3b302af6249ac59452bd584ce9bbcaf0a
Carol Schluter <cfmiller176@gmail.com>
2023-02-08 09:57:24 -0600

    input field processing and htm generation example

--- README.md
+++ README.md
@@ -25,3 +25,5 @@ SO THAT I can estimate costs of my next purchases
 ## Reference - used rescources
 
 -Button 12 from [https://getcssscan.com/css-buttons-examples](https://getcssscan.com/css-buttons-examples)
+
+-We consider the API response of [https://www.themealdb.com/] to be safe in a sense that we rely on displaying its content using innerHTML. Reasonable XSS (cross-site-scripting) protection was still added to the code.
--- food-lookup.js
+++ food-lookup.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.js-food-form');
+const inputField = document.querySelector('.js-food-input');
 const foodContainer= document.querySelector('.js-food-container');
 
 
@@ -8,7 +9,10 @@ console.log('JavaScript is running');
 
 function callback(event) {
     event.preventDefault();
-    alert('form got submitted');
+    const searchExpression = encodeURIComponent(inputField.value).replace('%20',' ');
+        inputField.value;
+    foodContainer.innerHTML =  `Form got submitted with content ${searchExpression}`
+    inputField.value = '';
 }
 
 form.addEventListener('submit', callback);
